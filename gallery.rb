1d369faa485ab544c1d168fa3952339a9a115238
davidjumble <hellodavidparkinson@gmail.com>
2017-12-08 21:17:07 +0000

    psuedo code for removing extra elements bug for generating html

--- gallery.rb
+++ gallery.rb
@@ -1,14 +1,17 @@
 
-
+html-array = []
 html = ""
 Dir.foreach("images/thumbs") {|image|
-html += "<div class='item'>
+html .push("<div class='item'>
 	<a href='?img=#{image}'>
 		<img src='images/thumbs/#{image}' width='420px' alt=''style='border: 3px solid #fff'/>
 	</a>
 </div>
-"}
+"})
 
-puts html
+for (int i = 0; i < html-array.length; i++){
+	if (i != 0 || i != 1)
+		html += html-array[i]
+}
 
 File.open("output", 'w') {|f| f.write(html) }
