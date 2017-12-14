ef760a2f882673f7b0174694db79cd585fb512b0
slummock <slummock@gmail.com>
2017-12-14 00:02:14 +0000

    added node build script for generating html from thumbs images,run: node build.js

---
+++ build.js
@@ -0,0 +1,23 @@
+var fs = require("fs");
+
+const thumbsdirectory = './images/thumbs/';
+const outputfile = './index.html'
+var indexsansgallery = './pre-gallery.html';
+
+
+
+fs.readFile(indexsansgallery, (err, page) => {
+    fs.readdir(thumbsdirectory, (err, directory) => {
+        var html = ""
+        directory.forEach(filename => {
+            console.log(filename);
+            html +=
+                "            <div class='item'>\n" +
+                "                <a href='?img=" + filename + "'>\n" +
+                "                <img src='images/thumbs/" + filename + "' width='420px' alt=''style='border: 3px solid #fff'/>\n" +
+                "                </a>\n" +
+                "            </div>\n"
+        });
+        fs.writeFile(outputfile, page.toString().replace('{INSERT-GALLERY-IMAGES}', html))
+    })
+  });
